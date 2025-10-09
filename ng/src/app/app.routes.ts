c53e9d7a9ff3ecbae5ee67038e2c6f5aa4b490b1
siddhartasr10 <siddhartasr9@live.com>
2025-10-09 12:26:16 +0200

    Added tailwind and configured tailwind and Angular
    MINOR
    
    Added tailwind and removed ssr files and tsconfig ssr files compilation declarations.

--- ng/src/app/app.routes.ts
+++ ng/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './views/about-me/about-me.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: "", component: AboutMeComponent },
+  { path: "**", redirectTo: "/", pathMatch: "full" }
+];
