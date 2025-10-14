5cc6530bdecb5a2f24bffede5a40c5f318ca7bc7
siddhartasr10 <siddhartasr9@live.com>
2025-10-14 15:22:33 +0200

    feat: added hero section and navbar
    Some decoration was also added but still much left.

--- ng/src/app/views/about-me/about-me.component.ts
+++ ng/src/app/views/about-me/about-me.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
