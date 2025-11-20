9780ac65db491c09b995d0ea71bddfeb4fbc4004
siddhartasr10 <siddhartasr9@live.com>
2025-11-20 12:33:56 +0100

    feat: Empezada view projects

--- ng/src/app/app.routes.ts
+++ ng/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AboutMeComponent } from './views/about-me/about-me.component';
+import { ProjectsComponent } from './views/projects/projects.component';
 
 
 export const routes: Routes = [
   { path: "", component: AboutMeComponent },
+  { path: "proyectos", component: ProjectsComponent },
   { path: "**", redirectTo: "/", pathMatch: "full" }
 ];
---
+++ ng/src/app/views/projects/projects.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-proyects',
+  imports: [ NavbarComponent ],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css'
+})
+export class ProjectsComponent {
+
+}
