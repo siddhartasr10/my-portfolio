7dde57aa4689cf6e6263a465af4023bc8079c33d
siddhartasr10 <siddhartasr9@live.com>
2026-05-19 13:06:27 +0200

    fix: Video pausing now checks for unselected cards
    Before if you stopped selecting any card and didn't select one in some
    cases it would try to pause the card '-1' which is the index for when no
    cards are selected. This error only polluted the console with error messages.

--- ng/src/app/views/projects/projects.component.ts
+++ ng/src/app/views/projects/projects.component.ts
@@ -228,7 +228,7 @@ export class ProjectsComponent {
   pauseCardVideoOnTime(): void {
     const lastHoverIdx = this.hoverIdx, lastCarrouselIdx = this.carrouselIdx;
 
-    setTimeout(() => (this.hoverIdx != lastHoverIdx || lastHoverIdx != this.carrouselIdx)
+    setTimeout(() => ((this.hoverIdx != lastHoverIdx || lastHoverIdx != this.carrouselIdx) && lastCarrouselIdx != -1)
       ? this.carrouselVideos[lastCarrouselIdx].nativeElement.pause()
       : null
       , this.VIDEO_PAUSE_TIMEOUT)
